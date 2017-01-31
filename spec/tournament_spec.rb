cc2357ed57e59327897d59ee8543bad370a0371d
Ali Al-Sheiba <m.sheiba@clickapps.co>
2017-01-31 15:22:48 +0530

    update tournamentcalendar test

--- spec/tournament_spec.rb
+++ spec/tournament_spec.rb
@@ -9,35 +9,39 @@ describe OptaSD::Soccer::TournamentCalendar do
   it "gets all tournaments" do
     match = OptaSD::Soccer::TournamentCalendar.new.get
 
-    expect(match.send(:build_uri).to_s).to include('tournamentcalendar')
-    expect(match.send(:build_uri).to_s).to_not include('active')
-    expect(match.send(:build_uri).to_s).to_not include('authorized')
+    url = match.send(:build_uri).to_s
+    expect(url).to include('tournamentcalendar')
+    expect(url).to_not include('active')
+    expect(url).to_not include('authorized')
     expect(match.data).to include("competition")
   end
 
   it "gets only active tournaments" do
     match = OptaSD::Soccer::TournamentCalendar.new.active.get
 
-    expect(match.send(:build_uri).to_s).to include('tournamentcalendar')
-    expect(match.send(:build_uri).to_s).to include('active')
-    expect(match.send(:build_uri).to_s).to_not include('authorized')
+    url = match.send(:build_uri).to_s
+    expect(url).to include('tournamentcalendar')
+    expect(url).to include('active')
+    expect(url).to_not include('authorized')
     expect(match.data).to include("competition")
   end
 
   it "gets only active and authorized for the user tournaments" do
     match = OptaSD::Soccer::TournamentCalendar.new.active.authorized.get
 
-    expect(match.send(:build_uri).to_s).to include('tournamentcalendar')
-    expect(match.send(:build_uri).to_s).to include('active')
-    expect(match.send(:build_uri).to_s).to include('authorized')
+    url = match.send(:build_uri).to_s
+    expect(url).to include('tournamentcalendar')
+    expect(url).to include('active')
+    expect(url).to include('authorized')
     expect(match.data).to include("competition")
   end
 
   it "gets tournament calendars by competition uuid" do
     match = OptaSD::Soccer::TournamentCalendar.new.competition(@valid_competition_id).get
 
-    expect(match.send(:build_uri).to_s).to include('tournamentcalendar')
-    expect(match.send(:build_uri).to_s).to include("comp=#{@valid_competition_id}")
+    url = match.send(:build_uri).to_s
+    expect(url).to include('tournamentcalendar')
+    expect(url).to include("comp=#{@valid_competition_id}")
     expect(match.data).to include("competition")
   end
 
